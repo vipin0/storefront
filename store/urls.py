0c206ddc941cfb62e9fccb913783dcc8901f2c7a
vipin0 <vipin6673@gmail.com>
2022-01-23 22:50:16 +0530

    added urls for routers

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,18 @@
-from .views import index
+from . import views
 from django.urls import path
+from rest_framework_nested import routers
 
-urlpatterns = [
-    path('playground/',index)
-]
+router = routers.DefaultRouter()
+router.register('products',views.ProductViewSet,basename='product')
+router.register('collections',views.CollectionViewSet,basename='collection')
+
+review_router = routers.NestedDefaultRouter(router,'products',lookup='product')
+review_router.register('reviews',views.ReviewViewSet,basename='product-review')
+
+urlpatterns = router.urls + review_router.urls
+# urlpatterns = [
+#     path('products/',views.product_list),
+#     path('products/<int:pk>',views.product_detail),
+#     path('collections/',views.collection_list),
+#     path('collections/<int:pk>',views.collection_detail),
+# ]
