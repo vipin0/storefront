00c586ce039edd15ebf7c4672f838fed4f2b658d
vipin0 <vipin6673@gmail.com>
2022-01-25 15:43:14 +0530

    to server static file on heroku

--- storefront/wsgi.py
+++ storefront/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storefront.settings')
 
 application = get_wsgi_application()
+
+application = StaticFilesHandler(get_wsgi_application())
