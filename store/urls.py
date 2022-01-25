163eb965b1da8121496f4df8b29d6994a3a3d6ea
vipin0 <vipin6673@gmail.com>
2022-01-25 13:54:30 +0530

    refactored urls

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from rest_framework_nested import routers
 from . import views
 
 router = routers.DefaultRouter()
+
 router.register('products', views.ProductViewSet, basename='product')
 router.register('collections', views.CollectionViewSet, basename='collection')
 router.register('carts', views.CartViewSet, basename='cart')
@@ -17,7 +18,9 @@ review_router.register('reviews', views.ReviewViewSet,
 carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
 carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
+
 urlpatterns = router.urls + review_router.urls + carts_router.urls
+
 # urlpatterns = [
 #     path('products/',views.product_list),
 #     path('products/<int:pk>',views.product_detail),
--- storefront/urls.py
+++ storefront/urls.py
@@ -20,18 +20,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import debug_toolbar
-
 from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
+from drf_yasg import openapi 
 
 schema_view = get_schema_view(
    openapi.Info(
       title="StoreFront API",
       default_version='v1',
-      description="Test description",
+      description="An e-commerce application API.",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      contact=openapi.Contact(email="vipin6673@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -40,9 +38,8 @@ schema_view = get_schema_view(
 admin.site.site_header = 'Store Front'
 admin.site.index_title = 'Admin'
 urlpatterns = [
-    # path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('store/',include('store.urls')),
     path('auth/',include('djoser.urls')),
