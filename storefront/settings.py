69e39aa813dfc7e34152e9b499c75ce25bc1f178
vipin0 <vipin6673@gmail.com>
2022-01-25 17:40:37 +0530

    remove userattributesimilarity password checking

--- storefront/settings.py
+++ storefront/settings.py
@@ -103,9 +103,9 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
